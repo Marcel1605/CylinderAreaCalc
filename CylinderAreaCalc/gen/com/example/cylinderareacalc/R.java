3b1a6328b018e21e870809dcd33aafccbd1fe35b
Konstantin <Konstantin@Konsti-Surface.local>
2015-10-06 23:20:24 +0200

    Infobutton berichtigt

--- CylinderAreaCalc/gen/com/example/cylinderareacalc/R.java
+++ CylinderAreaCalc/gen/com/example/cylinderareacalc/R.java
@@ -15,12 +15,11 @@ public final class R {
         public static final int ic_stat_name=0x7f020001;
     }
     public static final class id {
-        public static final int action_new=0x7f070009;
         public static final int button1=0x7f070006;
         public static final int button2=0x7f070007;
         public static final int editText1=0x7f070003;
         public static final int editText2=0x7f070005;
-        public static final int imageButton1=0x7f070008;
+        public static final int help_button=0x7f070008;
         public static final int textView1=0x7f070002;
         public static final int textView2=0x7f070001;
         public static final int textView3=0x7f070000;
@@ -34,7 +33,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int help_button_title=0x7f040001;
     }
     public static final class style {
         /** 
