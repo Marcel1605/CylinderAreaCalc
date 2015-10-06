87293c5c630dc666f66bd29e441341a25ffc8311
Marcel <marcel.keller1605@gmail.com>
2015-10-06 10:51:25 +0200

    menu ordner in res angelegt. dort eine actions.xml erstellt.
    in der main activity die oncreateoptionsmenu überschrieben...will nach
    dem schema
    http://www.tippscom.de/android-programmieren-tutorial-8-die-actionbar-und-wie-du-sie-anpassen-kannst/

--- CylinderAreaCalc/src/com/example/cylinderareacalc/MainActivity.java
+++ CylinderAreaCalc/src/com/example/cylinderareacalc/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
 
@@ -12,4 +13,12 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	 @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        MenuInflater inflater = getMenuInflater();
+	        inflater.inflate(R.menu.actions, menu);
+	 
+	        return super.onCreateOptionsMenu(menu); 
+	    }
 }
