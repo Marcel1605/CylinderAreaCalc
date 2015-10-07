c181765bea64dbc1f8b115112feb1f997d98c988
Konstantin <Konstantin@Konsti-Surface.local>
2015-10-07 15:32:29 +0200

    help activity

---
+++ CylinderAreaCalc/src/com/example/cylinderareacalc/HelpActivity.java
@@ -0,0 +1,17 @@
+package com.example.cylinderareacalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_help);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
