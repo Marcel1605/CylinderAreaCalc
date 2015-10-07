cd2f137b23168ff4080103a5e169c88c85485716
Konstantin <Konstantin@Konsti-Surface.local>
2015-10-07 17:29:44 +0200

    intent übergänge

--- CylinderAreaCalc/src/com/example/cylinderareacalc/MainActivity.java
+++ CylinderAreaCalc/src/com/example/cylinderareacalc/MainActivity.java
@@ -1,18 +1,52 @@
 package com.example.cylinderareacalc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	protected Button _button1 = null;
+	protected Button _button2 = null;
+	protected EditText _editText1 = null;
+	protected EditText _editText2 = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Referenzen auf View Elemente abfragen
+		_button1 = (Button) findViewById(R.id.button1);
+		_button2 = (Button) findViewById(R.id.button2);
+		_editText1 = (EditText) findViewById(R.id.editText1);
+		_editText2 = (EditText) findViewById(R.id.editText2);
+		
+		_button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+				startActivity(intent);
+			}
+		});
+		_button2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				_editText1.setText("");
+				_editText2.setText("");				
+			}
+		});
 	}
+		
 	
 	 @Override
 	    public boolean onCreateOptionsMenu(Menu menu) {
--- CylinderAreaCalc/src/com/example/cylinderareacalc/ResultActivity.java
+++ CylinderAreaCalc/src/com/example/cylinderareacalc/ResultActivity.java
@@ -2,14 +2,15 @@ package com.example.cylinderareacalc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
 
 public class ResultActivity extends Activity {
-
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
+	    setContentView(R.layout.activity_result);
 	    // TODO Auto-generated method stub
 	}
 
